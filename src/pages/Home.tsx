de8b757cf1a5cb67eae0253a5d4c3f85bf328025
DoniMartins <doni.martins.silva@gmail.com>
2025-03-05 18:04:01 +0000

    MAis uma tentativa

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,6 @@ const Home: React.FC = () => {
   return (
     <div className="relative min-h-screen w-screen overflow-hidden">
       {/* Content */}
-<<<<<<< HEAD
       <div className="relative z-10 h-screen flex flex-col items-center px-4 text-center">
         
         {/* Logo */}
@@ -23,12 +22,7 @@ const Home: React.FC = () => {
         </h1>
         <p className="absolute top-[40%] text-white text-xl md:text-3xl font-semibold">
 =======
-      <div className="relative z-10 h-screen flex flex-col items-center justify-center px-4 text-center">
-        <h1 className="text-white text-3xl md:text-5xl font-bold mb-6 max-w-3xl">
-          {t('home.mainText')}
-        </h1>
-        <p className="text-white text-xl md:text-3xl font-semibold">
->>>>>>> f514ce9 (adicionado ao googles)
+      
           {t('home.comingSoon')}
         </p>
       </div>
@@ -47,8 +41,5 @@ const Home: React.FC = () => {
   );
 };
 
-<<<<<<< HEAD
+
 export default Home;
-=======
-export default Home;
->>>>>>> f514ce9 (adicionado ao googles)
