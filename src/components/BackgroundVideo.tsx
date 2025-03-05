407afcd3eae39fde3424a2d5697a3b70ab3c9e63
DoniMartins <doni.martins.silva@gmail.com>
2025-03-05 17:35:19 +0000

    adicionado ao googles

--- src/components/BackgroundVideo.tsx
+++ src/components/BackgroundVideo.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 
 const BackgroundMedia: React.FC = () => {
@@ -42,9 +43,33 @@ const BackgroundMedia: React.FC = () => {
 
       {/* Overlay para efeito escurecido */}
       
+=======
+import React from 'react';
+
+const BackgroundVideo: React.FC = () => {
+  return (
+    <>
+      {/* Video Background */}
+      <video 
+        className="fixed inset-0 w-full h-full object-cover z-0"
+        autoPlay 
+        loop 
+        muted 
+        playsInline
+      >
+        <source src="https://res.cloudinary.com/doxbp6clc/video/upload/v1740764827/20250228_1743_Dynamic_Tool_Animation_remix_01jn6tfjqbe5atas3g7g117veq_ch5wdw.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      
+      {/* Overlay */}
+>>>>>>> f514ce9 (adicionado ao googles)
       <div className="fixed inset-0 bg-black bg-opacity-50 z-0"></div>
     </>
   );
 };
 
+<<<<<<< HEAD
 export default BackgroundMedia;
+=======
+export default BackgroundVideo;
+>>>>>>> f514ce9 (adicionado ao googles)
